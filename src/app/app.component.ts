0c306c0e54ca0f9927d2060d8e764d9dcc0cf2c3
Travis Payne <thpAGGIE910@gmail.com>
2018-02-17 17:22:42 -0600

    Initial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: 'pm-root',
+  template: `
+    <div>
+      <h1>{{pageTitle}}</h1>
+      <div>My First Component</div>
+    </div>
+  `
 })
 export class AppComponent {
-  title = 'app';
+  pageTitle: string = 'Acme Product Management';
 }
